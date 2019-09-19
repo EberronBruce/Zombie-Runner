e9bbad06cc4f6bee6e5d37c68b25f98998d5c135
Bruce Burgess <bruce@redravencomputing.com>
2019-09-18 23:19:41 -0600

    Trigger enemy animation states in code

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -37,10 +37,13 @@ public class EnemyAI : MonoBehaviour {
 	}
 
 	private void ChaseTarget() {
+		GetComponent<Animator>().SetBool("attack", false);
+		GetComponent<Animator>().SetTrigger("move");
 		navMeshAgent.SetDestination(target.position);
 	}
 
 	private void AttacktTarget() {
+		GetComponent<Animator>().SetBool("attack", true);
 		Debug.Log(name + " has seeked and is destroying " + target.name);
 	}
 
