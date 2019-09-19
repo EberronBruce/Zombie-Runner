562b6f6d01d838838ba5b7687f761f6d582781c9
Bruce Burgess <bruce@redravencomputing.com>
2019-09-18 18:08:29 -0600

    Created muzzle flash particle effect

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour {
 	[SerializeField] Camera FPCamera;
 	[SerializeField] float range = 100f;
 	[SerializeField] float damage = 25f;
+	[SerializeField] ParticleSystem muzzleFlash;
 
 	void Update() {
 		if(Input.GetButtonDown("Fire1")) {
@@ -16,8 +17,18 @@ public class Weapon : MonoBehaviour {
 	}
 
 	private void Shoot() {
+		PlayMuzzleFlash();
+		ProcessRaycast();
+
+	}
+
+	private void PlayMuzzleFlash() {
+		muzzleFlash.Play();
+	}
+
+	private void ProcessRaycast() {
 		RaycastHit hit;
-		if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range)) {
+		if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range)) {
 			Debug.Log("I hit this thing: " + hit.transform.name);
 			// TODO: add some hit effect for visual players
 			EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
@@ -26,8 +37,5 @@ public class Weapon : MonoBehaviour {
 		} else {
 			return;
 		}
-		
-
-		
 	}
 }
