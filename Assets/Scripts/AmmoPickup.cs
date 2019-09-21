a2dc66d0394522849334aed6da89fdd9244ded97
Bruce Burgess <bruce@redravencomputing.com>
2019-09-21 01:00:18 -0600

    Add ammo pickup variants

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour {
 
+	[SerializeField] int ammoAmount = 5;
+	[SerializeField] AmmoType ammoType;
+
 	private void OnTriggerEnter(Collider other) {
 		if(other.gameObject.tag == "Player") {
-			Debug.Log("Player did what players do");
+			FindObjectOfType<Ammo>().IncreasCurrentAmmo(ammoType, ammoAmount);
 			Destroy(gameObject);
 		}
 	}
